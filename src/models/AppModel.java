8c2775a1d35d9991689f7418eb4e5e0bc72436c9
Anastasia <nkarmazina1996@gmail.com>
2023-04-06 23:24:42 +0300

    Add new info in the classes

--- src/models/AppModel.java
+++ src/models/AppModel.java
@@ -1,4 +1,33 @@
 package models;
 
+import controller.CardController;
+import controller.CheckController;
+import controller.TaxController;
+import views.CardView;
+import views.CheckView;
+import views.TaxView;
+
 public class AppModel {
+    public void runPayCash() {
+        TaxView view = new TaxView();
+        TaxModel model = new TaxModel();
+        TaxController controller = new TaxController(model, view);
+        controller.runApp();
+    }
+
+    public void runPayCard() {
+        CardView view = new CardView();
+        TaxModel model = new TaxModel();
+        CardController controller = new CardController(model, view);
+        controller.runApp();
+    }
+    public void runPayCheck() {
+        CheckView view = new CheckView();
+        TaxModel model = new TaxModel();
+        CheckController controller = new CheckController(model, view);
+        controller.runApp();
+    }
+
+
+
 }
--- src/views/App.java
+++ src/views/App.java
@@ -1,4 +1,14 @@
 package views;
 
+import controller.AppController;
+import models.AppModel;
+
 public class App {
-}
+    public static void main(String[] args) {
+        AppModel model = new AppModel();
+        AppView view = new AppView();
+        AppController controller = new AppController(model, view);
+        controller.runApp();
+    }
+    }
+
